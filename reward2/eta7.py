5f029df23aa976d58836800b504835649a411d09
도경 <lee7198@naver.com>
2023-09-13 02:06:48 +0900

    faet eta8 직선경로 반영

---
+++ reward2/eta7.py
@@ -0,0 +1,28 @@
+# 주행 트렉에서 직선 포인트들
+straight_points = list(range(12, 38)) + list(range(48, 74)) + list(range(102, 142))
+
+
+def reward_function(params):
+    reward = 0
+
+    all_wheels_on_track = params["all_wheels_on_track"]
+    is_left_of_center = params["is_left_of_center"]
+    abs_steering = abs(params["steering_angle"])
+    waypoints = params["waypoints"]
+    closest_waypoints = params["closest_waypoints"]
+    next_point = waypoints[closest_waypoints[1]]
+
+    if is_left_of_center == True:
+        reward = 10
+    else:
+        reward = 1
+
+    if all_wheels_on_track == False:
+        reward = 1e-3
+
+    # 직선코스에서 조향각도 제한
+    if next_point[0] in straight_points:
+        if abs_steering > 5:
+            reward *= 0.5
+
+    return float(reward)
