cf0ac760332d0529b1765b34fa87202f2f5d635d
도경 <lee7198@naver.com>
2023-09-13 09:08:21 +0900

    faet eta8

--- reward2/eta7.py
+++ reward2/eta7.py
@@ -1,5 +1,7 @@
 # 주행 트렉에서 직선 포인트들
-straight_points = list(range(12, 38)) + list(range(48, 74)) + list(range(102, 142))
+straight_points = list(range(12, 38)) + list(range(102, 142))
+
+print(len(straight_points))
 
 
 def reward_function(params):
@@ -10,21 +12,32 @@ def reward_function(params):
     abs_steering = abs(params["steering_angle"])
     waypoints = params["waypoints"]
     closest_waypoints = params["closest_waypoints"]
-    next_point = waypoints[closest_waypoints[1]]
+    track_width = params["track_width"]
+    distance_from_center = params["distance_from_center"]
 
-    if all_wheels_on_track == False:
-        reward = 1e-3
+    next_point = waypoints[closest_waypoints[1]]
+    marker_1 = 0.1 * track_width
+    marker_2 = 0.25 * track_width
+    marker_3 = 0.5 * track_width
 
     # 직선코스에서 조향각도 제한
     if next_point[0] in straight_points:
-        if abs_steering < 7:
+        if abs_steering < 5:
             reward = 15
         else:
             reward = 1
     else:
         if is_left_of_center == True:
-            reward = 10
+            if distance_from_center <= marker_1:
+                reward = 5
+            elif distance_from_center <= marker_2:
+                reward = 10
+            elif distance_from_center <= marker_3:
+                reward = 15
         else:
             reward = 1
+
+    if all_wheels_on_track == False:
+        reward = 1e-3
 
     return float(reward)
