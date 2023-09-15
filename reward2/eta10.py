15d76c7f45ba4f489cf47c71006a553a2ab6189f
도경 <lee7198@naver.com>
2023-09-16 00:30:36 +0900

    faet 10

--- reward2/eta10.py
+++ reward2/eta10.py
@@ -9,30 +9,32 @@ def reward_function(params):
     is_left_of_center = params["is_left_of_center"]
     abs_steering = abs(params["steering_angle"])
     waypoints = params["waypoints"]
-    closest_waypoints = params["closest_waypoints"]
+    closest = params["closest_waypoints"]
     track_width = params["track_width"]
     distance_from_center = params["distance_from_center"]
     speed = params["speed"]
 
-    next_point = waypoints[closest_waypoints[1]]
+    next_point = max(closest[0], closest[1])
     marker_1 = 0.1 * track_width
     marker_2 = 0.25 * track_width
     marker_3 = 0.5 * track_width
 
-    # 직선코스에서 조향각도 제한
-    if is_left_of_center == True:
-        if distance_from_center <= marker_1:
-            reward = 0.1
-        elif distance_from_center <= marker_2:
-            reward = 0.5
-        elif distance_from_center <= marker_3:
+    if next_point in straight_points:
+        if abs_steering > 2:
             reward = 1
-
-    if next_point[0] in straight_points:
-        if abs_steering > 5:
-            reward *= 0.8
-        if speed >= 2.5:
+        if speed >= 3:
             reward += 1
+        elif speed <= 2.8:
+            reward -= 0.5
+    else:
+        # 직선코스에서 조향각도 제한
+        if is_left_of_center == True:
+            if distance_from_center <= marker_1:
+                reward += 0.1
+            elif distance_from_center <= marker_2:
+                reward += 0.5
+            elif distance_from_center <= marker_3:
+                reward += 1
 
     if all_wheels_on_track == False:
         reward = 1e-3
